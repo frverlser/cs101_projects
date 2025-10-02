a268e4fec419dbae1d0ef3c5a18b1efbecde890c
Sevinch <sevinch.qadamboyeva2007@gmail.com>
2025-10-02 12:34:09 +0500

    my first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+
+  
+ # This program asks for the user's name and then greets them.
+
+# 1. Display a prompt asking the user for their name.
+# The text the user types is stored in 'user_name'.
+user_name = input("Please enter your name: ")
+
+# 2. Display a personalized greeting using the stored name.
+print("Hello, " + user_name + "! Welcome to CS101.")
